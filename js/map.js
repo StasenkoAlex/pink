90fb518b6df578c4194b50cd32c23afef460d528
AlexStasenko <adubovika@gmail.com>
2017-09-15 13:14:37 +0300

    add googleAPI to map

--- js/map.js
+++ js/map.js
@@ -1,23 +1,14 @@
-function initialize() {
-  var mapCanvas = document.getElementById('map');
-
-  var mapOptions = {
-    scrollwheel: false,
-    center: new google.maps.LatLng(59.9388218,30.3230753),
-    zoom: 17,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-
-  var image = "./img/map-marker.png";
-  var myLatLng = new google.maps.LatLng(59.9388218,30.3230753);
-  var pinkMarker = new google.maps.Marker({
-    position: myLatLng,
+function initMap() {
+  var uluru = {lat:59.9388218, lng:30.3230753};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
     map: map,
     icon: {
-      url: image/*,
-      scaledSize: new google.maps.Size(36,36)*/
+      url: "./img/map-marker.png"
     }
   });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
